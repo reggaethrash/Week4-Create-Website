492e2af9ceb5a7ac2b6288e22dceb48f2ec69981
reggaethrash <52338860+reggaethrash@users.noreply.github.com>
2019-07-20 16:32:40 +0800

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,130 @@
+let products =[
+    {
+        pname:"<h1>Heart Evangelista</h2>",
+        price:"₱ 1000",
+        pcode:"Model-001",
+        description:"Even before starring in hit television dramas such as Legacy and Beautiful Strangers, Heart has been in the limelight since her early teens. A passionate altruist, she believes that everyone should face their hardships with confidence and determination.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Heart_Evangelista",
+        imgurl:"https://images.squarespace-cdn.com/content/v1/5c9edcd892441bd4a9750a0f/1559048822327-E91OCI7DJ03J4UHRUEK5/ke17ZwdGBToddI8pDm48kPEKB-lf2MwUihDMd7A-i817gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTm1v6GcKqh6mrhfxzW2tqo79WCJDTAVZlSo04Z_zTsgPLdK7On3st_wfYOn8J0ckcF/Tanduay+Day+2-141.jpg?"
+    },
+    {
+        pname:"<h1>Bela Padilla</h1>",
+        price:"₱ 1000",
+        pcode:"Model-002",
+        description:"Best known for her roles in FPJ’s Ang Probinsyano, Camp Sawi and 100 Tula Para Kay Stella, Bela is a talented actress with a passionate and fearless personality. She’s supports mental health awareness and firmly believes that people should be unafraid to talk about their experiences.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Bela_Padilla",
+        imgurl:"https://images.squarespace-cdn.com/content/v1/5c9edcd892441bd4a9750a0f/1559048986371-LHQM3AQSOPCKBSTE1FRE/ke17ZwdGBToddI8pDm48kFEdmHHsYhY0TepCfyQ4ElR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmzJBBHkqvGAibxByi2-xi2eeKRwiGF7fUSfkNof97kZcD40aVOerT5i_TLsaSZLs/Tanduay++Calendar+2019+Bella+Padilla84513.jpg?format=750w"
+    },
+    {
+        pname:"<h1>Erich Gonzales</h1>",
+        price:"₱ 1000",
+        pcode:"Model-003",
+        description:"From her first leading role in hit TV series Katorse all the way to her latest in The Blood Sisters, Erich’s prominent and versatile acting career continues to stay strong through the years. She believes that everyone should do their best in becoming a better person and be unafraid to choose their own happiness.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Erich_Gonzales",
+        imgurl:"https://images.squarespace-cdn.com/content/v1/5c9edcd892441bd4a9750a0f/1559048712637-EAZNP8JA24A7WTV61JXW/ke17ZwdGBToddI8pDm48kF9aEDQaTpZHfWEO2zppK7Z7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UX7HUUwySjcPdRBGehEKrDf5zebfiuf9u6oCHzr2lsfYZD7bBzAwq_2wCJyqgJebgg/TANDUAY+201853376.jpg?format=750w"
+    },
+    {
+        pname:"<h1>Ellen Adarna</h1>",
+        price:"₱ 1000",
+        pcode:"Model-004",
+        description:"Having made her acting debut in comedy sketch TV show Bubble Gang back in 2010, Ellen has since become famous for starring in television dramas such as Pasión de Amor and Langit Lupa. With her alluring personality and confidence, she embraces her imperfections and encourages other women to do so as well.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Ellen_Adarna",
+        imgurl:"https://images.squarespace-cdn.com/content/v1/5c9edcd892441bd4a9750a0f/1559048917932-6V0BCMASB54BET91J8RX/ke17ZwdGBToddI8pDm48kLSERMgCVymnItqhne5EfYV7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmMCg6RGY8TrcVSOIk4QoDPnvjthEs8TAhVmYN7i_-QaEW7L_Q40KNxq4S2FLq3V0y/Tanduay+Ellen+Adarna+Calendar+201627474.jpg?format=750w"
+    },
+    {
+        pname:"<h1>Jessy Mendiola</h1>",
+        price:"₱ 1000",
+        pcode:"Model-005",
+        description:"Best known for her leading roles in Maria Mercedes and The Girl in the Orange Dress, Jessy has left her mark in the industry with her impressive and bold acting. She firmly believes in staying confident and loving yourself and your flaws despite life's challenges.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Jessy_Mendiola",
+        imgurl:"https://images.squarespace-cdn.com/content/v1/5c9edcd892441bd4a9750a0f/1559048613834-TGHAAQUTHCRYS1U7TXSK/ke17ZwdGBToddI8pDm48kDsmujvCxGgFgBWj31Y2d6R7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmmbLecD_NwfjqNxjfNDWHyATeL23bceyBtuX4nDs5VF5LtFSR0qFcA7plit5Vqms8/TANDUAY_0319+FA.jpg?format=750w"
+    },
+    {
+        pname:"<h1>Megan Fox</h1>",
+        price:"₱ 1000",
+        pcode:"Model-006",
+        description:"Megan Denise Fox (born May 16, 1986) is an American actress and model. She began her acting career in 2001, with several minor television and film roles, and played a regular role on the Hope & Faith television sitcom. In 2004, she made her film debut with a role in the teen comedy Confessions of a Teenage Drama Queen.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Megan_Fox",
+        imgurl:"https://i.pinimg.com/originals/5d/72/a6/5d72a63100b1d5fd734002e5e5defc93.jpg"
+    },
+    {
+        pname:"<h1>Gigi Hadid</h1>",
+        price:"₱ 1000",
+        pcode:"Model-007",
+        description:"Hadid has walked for numerous high fashion brands over her career so far, and gained 35 appearances on international Vogue magazine covers over only four years of her professional career.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Gigi_Hadid",
+        imgurl:"http://www.mtv.com.au/sites/default/files/styles/vimn_image_embed/public/mtv_au/articles/2015/02/04/gigi_hadid_bikini.jpg?itok=Tyrc_1ny"
+    },
+    {
+        pname:"<h1>Kendall Jenner</h1>",
+        price:"₱ 1000",
+        pcode:"Model-008",
+        description:"Jenner first appeared in the E! reality television show Keeping Up with the Kardashians, and is now also a model in a movement dubbed the Instagirl era, where models are chosen for their online presence and social media following.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Kendall_Jenner",
+        imgurl:"https://akns-images.eonline.com/eol_images/Entire_Site/201839/rs_1080x1350-180409135358-1080x1350-Kendall-Jenner-Bikini.jl.040918.jpg?fit=inside|900:auto&output-quality=90"
+    },
+    {
+        pname:"<h1>Kylie Jenner</h1>",
+        price:"₱ 1000",
+        pcode:"Model-009",
+        description:"Kylie Kristen Jenner (born August 10, 1997)[4] is an American media personality, model, businesswoman, and socialite. She has starred in the E! reality television series Keeping Up with the Kardashians since 2007 and is the founder and owner of cosmetic company Kylie Cosmetics.",
+        manufacturer:"",
+        stocks:"",
+        supplier:"",
+        expdate:"",
+        url:"https://en.wikipedia.org/wiki/Kylie_Jenner",
+        imgurl:"https://assets.teenvogue.com/photos/5c4237649eeaf112b818b840/master/w_1600%2Cc_limit/kylie.jpg"
+    }
+]
+let output = "";
+products.forEach(function(item){
+    if (item.imgurl == undefined)
+    {
+        item.imgurl = "https://via.placeholder.com/300"
+    }
+    output += `<div class ='item'> 
+    <div class="card" style="width: 18rem;">
+    <img src="${item.imgurl}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${item.pname}</h5>
+      <p class="card-text">${item.description}</p>
+      <a href="${item.url}" class="btn btn-primary">${item.price}</a>
+    </div>
+  </div>
+    </div>
+    
+    `
+})
+document.getElementById('container').innerHTML = output
